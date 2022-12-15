c792e2382a12599105001b9c94dd97cdb24d9381
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2022-12-15 21:54:19 +0530

    jshndson

---
+++ main.js
@@ -0,0 +1,59 @@
+//  const parent  ={
+//     name:"shivaprasad",
+//     college:"TKR college of eng.tech",
+//      data(){
+//        return ( " hai" + ""+ this.name+"from"+this.college)
+//      }
+    
+//  }
+//  const child ={
+//     __proto__:parent
+//  }
+//  console.log(child)
+
+// arr1=[1,2,3,4,5,6,7,8];
+// arr2=[2,4,6,8,10];
+// arr3=[1,3,5,7,9];
+//  const  arraysum={
+//    plus:function(arr){
+//       let sum=0;
+//       for(let i=0;i<arr.length;i++){
+//          sum+=arr[i];
+//       }
+//       console.log(sum);
+//    }
+//  }
+//  Array.__proto__=arraysum;
+
+//  Array.plus(arr1);
+//  Array.plus(arr2);
+
+//  Array.plus(arr3);
+  
+// function Person ( name,age) {
+//    this.name = name, 
+//    this.age = age
+// }
+
+// // creating objects
+// const person1 = new Person( "shiva",23);
+
+
+// // adding property to constructor function
+// Person.prototype.gender = 'male';
+
+// // prototype value of Person
+// console.log(Person.prototype);
+
+// // inheriting the property from prototype
+// console.log(person1);
+// console.log(person1.gender);
+
+//  var myobj ={
+//    name : "shivaprasad",
+//    course : "fullstack",
+//    location : "hyd",
+//  }
+//  let result = Object.keys(myobj);
+//  console.log(result);
+
